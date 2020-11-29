a40ed84b1102a67d802be2d58b4ddf1fd4bf8070
Dung Zoe <vomydung.zoey@gmail.com>
2020-11-29 16:19:30 +0700

    Color Box
    - Set random color for a box
    - Handle box click

---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './ColorBox.scss';
+
+ColorBox.propTypes = {
+    
+};
+
+function getRandomColor () {
+    const COLOR_LIST = ['deeppink', 'green', 'goldenrod', 'darkblue'];
+    const randomIndex = Math.trunc(Math.random()*4);
+    return COLOR_LIST[randomIndex];
+}
+
+function ColorBox() {
+    
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('box_color') || 'deeppink';
+        console.log(initColor);
+        return initColor;
+    });
+
+    function handleBoxClick() {
+        const newColor = getRandomColor();
+        setColor(newColor);
+
+        localStorage.setItem('box_color', newColor);
+    }
+
+    return (
+        <div 
+            className="color-box" 
+            style={{backgroundColor: color}}
+            onClick={handleBoxClick}
+        >
+        </div>
+    );
+}
+
+export default ColorBox;
